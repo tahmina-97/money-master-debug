fa3ea0755307d027fccd389d5cba89159ddbaaa9
Tahmina Ria <tahminariya459@gmail.com>
2022-09-14 21:51:35 +0600

    count solved

--- script.js
+++ script.js
@@ -101,7 +101,9 @@ const closeModal = () => {
 
 const start = () => {
   // If already started, do not start again
-  if (startTime) return;
+  if (startTime) {
+    return;
+  }
 
   let count = 3;
   countdownOverlay.style.display = "flex";
@@ -110,10 +112,10 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
